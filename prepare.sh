f7d455a6d2589489e76106b12ffe843172e4521b
diotech174 <diogenes174@gmail.com>
2021-12-15 11:31:39 -0300

    update

--- prepare.sh
+++ prepare.sh
@@ -36,8 +36,8 @@ if [ "" = "$GCHROME__OK" ]; then
 
   	echo "No $GCHROME_PKG. Setting up $GCHROME_PKG."
   	wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -P /home/$USER/Downloads/google-chrome-stable.deb
-  	sudo apt install /home/$USER/google-chrome-stable*
-  	chmod 777 /home/$USER/google-chrome-stable*
+  	sudo apt install /home/$USER/Downloads/google-chrome-stable.deb
+  	chmod 777 /home/$USER/Downloads/google-chrome-stable*
   	rm /home/$USER/google-chrome-stable*
 fi
 
